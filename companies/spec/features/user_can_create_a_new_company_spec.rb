e54b900d940a421364460c1646a55e91f936bfee
Teresa Knowles <teresa07moreno@gmail.com>
2019-07-20 17:19:23 -0600

    Test for creating a new company

---
+++ companies/spec/features/user_can_create_a_new_company_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.feature "Company Create", type: :feature do
+  describe 'As a visitor' do
+    before :each do
+      visit '/'
+      click_on("Add Company")
+    end
+
+    describe 'when I click on Add Company' do
+      it 'takes me to the create a company form' do
+        expect(current_path).to eq(new_company_path)
+      end
+
+      it 'I can see all the fields needed to create a Company' do
+        expect(page).to have_field("Name")
+        expect(page).to have_field("City")
+        expect(page).to have_field("State")
+        expect(page).to have_field("Description")
+        expect(page).to have_field("Founded Date")
+      end
+    end
+  end
+end
