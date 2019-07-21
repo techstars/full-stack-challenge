674a3665c8cc967395a6db02791c0df56e5ac885
Teresa Knowles <teresa07moreno@gmail.com>
2019-07-20 20:26:41 -0600

    Change location to city and state and add founded_date to Company

--- companies/spec/models/company_spec.rb
+++ companies/spec/models/company_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Company, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
-    it { should validate_presence_of :location }
-    it { should validate_presence_of :description }
+    it { should validate_presence_of :city}
+    it { should validate_presence_of :state }
+    it { should validate_presence_of :founded_date }
   end
 end
