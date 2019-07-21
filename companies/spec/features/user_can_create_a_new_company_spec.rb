78d407d8cabfd80989e2adfe38d5b7ea947a3eac
Teresa Knowles <teresa07moreno@gmail.com>
2019-07-20 21:55:15 -0600

    Add test for sad path of create action

--- companies/spec/features/user_can_create_a_new_company_spec.rb
+++ companies/spec/features/user_can_create_a_new_company_spec.rb
@@ -21,20 +21,47 @@ RSpec.feature "Company Create", type: :feature do
         expect(page).to have_field("date-picker")
       end
 
-      it 'can create a new company' do
+      context "when all fields are filled correctly" do
+        it 'can create a new company' do
 
-        fill_in "company_name", with: "Test Name"
-        fill_in "company_city", with: "Denver"
-        fill_in "company_state", with: "CO"
-        fill_in "company_description", with: Faker::Lorem.paragraphs(7).join
-        fill_in "date-picker", with: Faker::Date.backward(14000)
+          fill_in "company_name", with: "Test Name"
+          fill_in "company_city", with: "Denver"
+          fill_in "company_state", with: "CO"
+          fill_in "company_description", with: Faker::Lorem.paragraphs(7).join
+          fill_in "date-picker", with: Faker::Date.backward(14000)
 
-        click_on "Save"
+          click_on "Save"
 
-        company = Company.last
-        expect(company.name).to eq("Test Name")
-
+          company = Company.last
+          expect(company.name).to eq("Test Name")
+        end
       end
+
+      context "when fields are not filled correctly" do
+        it 'cannot create a new company' do
+          fill_in "company_name", with: "Test Name"
+          fill_in "company_city", with: "Denver"
+          fill_in "company_state", with: "CO"
+
+          click_on "Save"
+          expect(Company.count).to eq(0)
+        end
+
+        it 'renders the new company form again' do
+          fill_in "company_name", with: "Test Name"
+          fill_in "company_city", with: "Denver"
+          fill_in "company_state", with: "CO"
+
+          click_on "Save"
+          
+          expect(page).to have_button('Save')
+          expect(page).to have_field("Description", with: "")
+          expect(page).to have_content("Description can't be blank")
+          expect(page).to have_content("Founded date can't be blank")
+        end
+      end
+
+
     end
   end
 end
