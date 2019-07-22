09d43b1bc893a5c9ee6719fb009b386ef4a52859
rhysgoehring <rhysgoehring@gmail.com>
2019-07-22 11:03:54 -0600

    configured rspec to use shoulda-matchers and factory bot methods. setup database cleanerto truncate tables before tests

--- techstars-api/spec/rails_helper.rb
+++ techstars-api/spec/rails_helper.rb
@@ -1,5 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'database_cleaner'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
@@ -30,6 +31,16 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+# Configure Shoulda Matchers to use rspec as the test framework & full matcher libraries for rails:
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -38,6 +49,23 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+
+  # Include Factory Bot Methods:
+  config.include FactoryBot::Syntax::Methods
+
+  # Start by truncating all tables but use the faster transaction strategy the rest of the time:
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  # Start using transaction strategy when examples are run:
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
