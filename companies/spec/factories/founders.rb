392a224cbe0a2782c8cefaed870556a984a8cbb8
Teresa Knowles <teresa07moreno@gmail.com>
2019-07-22 00:10:08 -0600

    Add test for founders model

---
+++ companies/spec/factories/founders.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :founder do
+    name { "MyString" }
+    title { "MyString" }
+    company { nil }
+  end
+end
--- companies/spec/models/company_spec.rb
+++ companies/spec/models/company_spec.rb
@@ -7,4 +7,8 @@ RSpec.describe Company, type: :model do
     it { should validate_presence_of :state }
     it { should validate_presence_of :founded_date }
   end
+
+  describe "associations" do
+    it {should have_many :founders}
+  end
 end
