14e4de5c125b9b5ece672e71cf0d4d661834c911
rhysgoehring <rhysgoehring@gmail.com>
2019-07-22 13:38:16 -0600

    fixed syntax error, test suite running

--- techstars-api/spec/requests/companies_spec.rb
+++ techstars-api/spec/requests/companies_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-Rspec.describe 'Techstars Assessment API', type :request do
+RSpec.describe 'Techstars Assessment API', type: :request do
   # Initialize test data:
   let!(:companies) { create_list(:company, 12) }
   let(:company_id) { companies.first.id }
@@ -48,3 +48,5 @@ Rspec.describe 'Techstars Assessment API', type :request do
   end
 
 
+
+end
