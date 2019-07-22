700a2e82c82befecd179c8927adab3226c1535e3
rhysgoehring <rhysgoehring@gmail.com>
2019-07-22 12:11:16 -0600

    wrote company model spec test, tests association with founders model and validates fields

--- techstars-api/spec/models/company_spec.rb
+++ techstars-api/spec/models/company_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association Test
+  it { should have_many(:founders).dependent(:destroy) }
+
+  # Validation tests
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:city) }
+  it { should validate_presence_of(:state) }
+  it { should validate_presence_of(:description) }
+  it { should validate_presence_of(:date_founded) }
+  it { should validate_presence_of(:location) }
 end
