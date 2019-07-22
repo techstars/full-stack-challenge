d75ee35f099dff3a02230fc7b82d014bac9f0111
Teresa Knowles <teresa07moreno@gmail.com>
2019-07-21 21:12:36 -0600

    Test for show action

---
+++ companies/spec/features/companies_show_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.feature "Company show page", type: :feature do
+  describe 'As a visitor' do
+
+    describe 'When I click on more... for a company' do
+      before :each do
+        @company = build(:company)
+        visit '/'
+        within "#company-#{@company.id}" do
+          click_on "more..."
+        end
+      end
+      it "takes me to the company's show page" do
+        expect(current_path).to eq(company_path(@company))
+      end
+      it 'shows the name of the company' do
+        expect(page).to have_content(@company.name)
+      end
+      it "shows the company's description" do
+        expect(page).to have_content(@company.description)
+      end
+      it 'shows the founded date' do
+        expect(page).to have_content(@company.founded_date)
+      end
+      it 'shows the city and state' do
+        expect(page).to have_content(@company.city)
+        expect(page).to have_content(@company.state)
+      end
+      it 'shows a button to edit' do
+        expect(page).to have_button("Edit")
+      end
+      it 'shows a button to delete' do
+        expect(page).to have_button("Delete")
+      end
+    end
+  end
+end
