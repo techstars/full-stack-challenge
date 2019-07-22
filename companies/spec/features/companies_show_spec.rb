0798a7259cbc4458f3509ab99c8fd9ecfff69ab3
Teresa Knowles <teresa07moreno@gmail.com>
2019-07-22 00:27:31 -0600

    Test for list of founders in company's show page

--- companies/spec/features/companies_show_spec.rb
+++ companies/spec/features/companies_show_spec.rb
@@ -33,6 +33,16 @@ RSpec.feature "Company show page", type: :feature do
       it 'shows a button to delete' do
         expect(page).to have_button("Delete")
       end
+      it 'shows list of founders' do
+        founders = create_list(:founder, 3)
+        @company.founders << founders
+        expect(page).to have_content("Founders")
+        founders.each do |founder|
+          expect(page).to have_content(founder.name)
+          expect(page).to have_content(founder.title)
+        end
+
+      end
     end
   end
 end
