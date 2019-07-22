c8f6d7576c0fd2368ed0e735d9cc209ac3ac7d50
rhysgoehring <rhysgoehring@gmail.com>
2019-07-22 12:34:23 -0600

    wrote company and founder models, all spec tests passing

--- techstars-api/app/models/company.rb
+++ techstars-api/app/models/company.rb
@@ -1,2 +1,6 @@
 class Company < ApplicationRecord
+  # Model Association
+  has_many :founders, dependent: :destroy
+
+  validates_presence_of :name, :city, :state, :description, :date_founded, :location
 end
--- techstars-api/app/models/founder.rb
+++ techstars-api/app/models/founder.rb
@@ -1,3 +1,6 @@
 class Founder < ApplicationRecord
   belongs_to :company
+
+  # Validation
+  validates_presence_of :name, :title
 end
