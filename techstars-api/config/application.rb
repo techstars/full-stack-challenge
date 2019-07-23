384fd2359f0b507d28e7744babf94436d67bda75
rhysgoehring <rhysgoehring@gmail.com>
2019-07-23 12:34:34 -0600

    enabled cors

--- techstars-api/config/application.rb
+++ techstars-api/config/application.rb
@@ -1,4 +1,4 @@
-require_relative 'boot'
+require_relative "boot"
 
 require "rails"
 # Pick the frameworks you want:
@@ -10,6 +10,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
+
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
