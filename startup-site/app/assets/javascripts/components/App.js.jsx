fe3732ab989ff996970e70c70e6af9650e1d5520
aimeezawacki <aimeezawacki@maxreservations.com>
2019-08-25 17:16:01 -0600

    wired up create company form to state, switching gears to work on post route.

--- startup-site/app/assets/javascripts/components/App.js.jsx
+++ startup-site/app/assets/javascripts/components/App.js.jsx
@@ -1,25 +1,51 @@
 class App extends React.Component{
   constructor(props) {
-     super(props)
-     this.state = {
-       companies: this.props.companies || []
-     }
-
+    super(props)
+    this.state = {
+      companies: this.props.companies || [],
+      showForm: false,
+      newCompany: {
+        name: '',
+        city: '',
+        state: '',
+        short_description: '',
+        long_description: '',
+        image_url: ''
+      }  
+    }
      this.toggleAddCompany = this.toggleAddCompany.bind(this)
+     this.onChange = this.onChange.bind(this)
+     this.onSubmit = this.onSubmit.bind(this)
   }
   render() {
-    // console.log("props", this.props)
-    return (
-      <div className="container-fluid site">
-        <Navigation toggleAddCompany={this.toggleAddCompany}/>
+    return this.state.showForm 
+      ? <CreateCompany 
+          toggleAddCompany={this.toggleAddCompany}
+          onChange={this.onChange}
+          onSubmit={this.onSubmit}
+        /> 
+      : <div className="container-fluid site">
+          <Navigation toggleAddCompany={this.toggleAddCompany}/>
           <div className="col-sm-12">
             <Companies companies={this.state.companies}/>
           </div>
-      </div>
-    )
+        </div>    
   }
 
-  toggleAddCompany(event) {
-    console.log("on add company", event.target)
+  toggleAddCompany() {
+    this.setState({showForm: !this.state.showForm})
+  }
+
+  onChange(event) {
+    var newCompany = {...this.state.newCompany}
+    newCompany[event.target.name] = event.target.value
+    this.setState({
+      newCompany: newCompany
+    })
+  }
+
+  onSubmit() {
+    console.log("submit this", this.state.newCompany)
+    
   }
 }
--- startup-site/app/assets/javascripts/components/CreateCompany.js.jsx
+++ startup-site/app/assets/javascripts/components/CreateCompany.js.jsx
@@ -5,10 +5,36 @@ class CreateCompany extends React.Component {
 
   render() {
     return (
-      <form className="nav row">
-        <label>Company Name</label>
-        <input type="text"/>
-      </form>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12">
+            <button className="close" onClick={this.props.toggleAddCompany}>X</button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-12">
+            <h1>Create A Company</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-12">
+            <label>Company Name</label>
+            <input name="name" type="text" onChange={this.props.onChange}/>
+            <label>City</label>
+            <input name="city" type="text" onChange={this.props.onChange}/>
+            <label>State</label>
+            <input name="state" type="text" onChange={this.props.onChange}/>
+            <label>Short Description</label>
+            <input name="short_description" type="text" onChange={this.props.onChange}/>
+            <label>Long Description</label>
+            <input name="long_description" type="text" onChange={this.props.onChange}/>
+            <label>Image URL</label>
+            <input name="image_url" type="text" onChange={this.props.onChange}/>
+          </div>
+            <button onClick={this.props.onSubmit}>Submit</button>
+        </div>
+      </div>
+      
     )
   }
 }
