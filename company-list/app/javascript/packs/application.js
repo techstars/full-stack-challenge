c8c86f9057b7f71b9af9157d85ed636b68c816bb
ibenthinkin <benjamin.reilly@gmail.com>
2019-08-27 14:44:47 -0600

    add react-router

--- company-list/app/javascript/packs/application.js
+++ company-list/app/javascript/packs/application.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import App from '../components/App';
+import AddCompanyForm from '../components/AddCompanyForm';
 
 document.addEventListener('DOMContentLoaded', () => {
-    render(<App />, document.querySelector('#root'));
+    render(
+        <Router>
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route exact path="/companies" component={App} />
+            </Switch>
+        </Router>,
+        document.querySelector('#root'),
+    );
 });
