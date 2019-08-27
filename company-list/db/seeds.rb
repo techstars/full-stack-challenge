021ef5af349775d2dce6692c8df7925e4a27098c
ibenthinkin <benjamin.reilly@gmail.com>
2019-08-27 13:29:54 -0600

    seed database

--- company-list/db/seeds.rb
+++ company-list/db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+12.times do |index|
+    Company.create(
+        name: Faker::Company.name,
+        description: Faker::Company.catch_phrase,
+        founded_date: Faker::Date.between(from: 100.years.ago, to: Date.today),
+        city: Faker::Address.city,
+        state: Faker::Address.state
+    )
+
+    Founder.create(
+        name: Faker::Name.name, 
+        title: "Founder and #{Faker::Job.position}",
+        company_id: index + 1
+    )
+
+end    
