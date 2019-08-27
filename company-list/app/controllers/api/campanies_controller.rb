e12ee15d572b9eff708d7ed1b51ff7c4716ff1d8
ibenthinkin <benjamin.reilly@gmail.com>
2019-08-27 13:39:21 -0600

    make controllers for Companies and Founders

---
+++ company-list/app/controllers/api/campanies_controller.rb
@@ -0,0 +1,39 @@
+class Api::CompaniesController < ApplicationController
+  respond_to :json
+
+  def index
+    respond_with Company.all()
+  end
+
+  def show
+    respond_with Company.find(params[:id])
+  end
+
+  def create
+    respond_with :api, Company.create(company_params)
+  end
+
+  def destroy
+    respond_with Company.destroy(params[:id])
+  end
+
+  def update
+    company = Company.find(params['id'])
+    company.update(company_params)
+    respond_with Company, json: company
+  end
+
+  private
+
+  def company_params
+    params.require(:event).permit(
+      :id,
+      :name,
+      :description,
+      :founded_date,
+      :city,
+      :host,
+      :published
+    )
+  end
+end
