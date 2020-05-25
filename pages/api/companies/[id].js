c78804f308d96fe4a100289a22f81130e4b933d3
Skyler Williams <skyler.zee.williams@gmail.com>
2020-05-25 14:09:03 -0600

    Add PUT functionality to companies detail endpoint

--- pages/api/companies/[id].js
+++ pages/api/companies/[id].js
@@ -2,6 +2,7 @@ import db from '../../../database/database';
 
 export default async (req, res) => {
   const {
+    body,
     method,
     query: { id },
   } = req;
@@ -26,8 +27,35 @@ export default async (req, res) => {
           }
         });
       });
+    case 'PUT':
+      return new Promise((resolve, reject) => {
+        const bodyData = JSON.parse(body);
+        const updateStatement = db.prepare(`UPDATE companies
+          SET name = ?, description = ?, city = ?, state = ?, date_founded = ?
+          WHERE id = ?`);
+        updateStatement.run(
+          [bodyData.name,
+          bodyData.description,
+          bodyData.city,
+          bodyData.state,
+          bodyData.dateFounded,
+          id],
+          (err) => {
+          if (err) {
+            res.status(400).json({ "error": err.message });
+            resolve();
+          } else {
+            if (updateStatement.lastID && updateStatement.changes > 0) {
+              res.status(200).json({id: updateStatement.lastID});
+            } else {
+              res.status(404).end();
+            }
+            resolve();
+          }
+        });
+      });
     default:
-      res.setHeader('Allow', ['GET'])
+      res.setHeader('Allow', ['GET', 'PUT'])
       res.status(405).end(`Method ${method} Not Allowed`)
   }
 }
