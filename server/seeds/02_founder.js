5c2f834272d8969e6aedf722f31829682eb4ef46
Coleman Imhoff <colemanimhoff@gmail.com>
2020-05-30 15:12:41 -0600

    typo with alter sequence in founder seed file

--- server/seeds/02_founder.js
+++ server/seeds/02_founder.js
@@ -2,7 +2,7 @@
 const founders = require('../fixtures/founders.json')
 
 exports.seed = (knex) => {
-  return knex.raw('ALTER SEQUENCE company_id_seq RESTART WITH 13')
+  return knex.raw('ALTER SEQUENCE founder_id_seq RESTART WITH 1')
     .then(() => knex('founder').del())
     .then(() => knex('founder').insert(founders))
 }
