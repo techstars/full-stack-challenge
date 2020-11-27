00a57c4bd893a87a4f6a4ad4f0e137f26b46f77d
Chris Williams <Chris@Chriss-MacBook-Pro-2.local>
2020-11-27 16:06:48 -0700

    adds state to form fields and form submit button/handling

--- client/src/components/AddCompany.jsx
+++ client/src/components/AddCompany.jsx
@@ -1,6 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddCompany = () => {
+  const[name, updateName] = useState('');
+  const[city, updateCity] = useState('');
+  const[state, updateState] = useState('');
+  const[founded, updateFounded] = useState('');
+  const[description, updateDescription] = useState('');
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault()
+
+    const formData = {
+      name,
+      city,
+      state,
+      founded,
+      description,
+    }
+
+    // check if any fields are empty
+    for (const field in formData) {
+      if (!formData[field]) {
+        alert('Please complete form before submitting')
+        return
+      }
+    }
+
+    // API put request will go here
+  }
+
   return (
     <div className="add-company-wrapper">
 
@@ -9,33 +37,59 @@ const AddCompany = () => {
         <div className="form-line-one">
           <label>
             Name: <br/>
-            <input type="text" name="name" className="form-input line-one-input"/>
+            <input
+              type="text"
+              name="name"
+              className="form-input line-one-input"
+              onChange={e=> updateName(e.target.value)}
+            />
           </label>
         </div>
 
         <div className="form-line-two">
           <label className="line-two-label">
             City: <br/>
-            <input type="text" name="city" className="form-input line-two-input" />
+            <input
+              type="text"
+              name="city"
+              className="form-input line-two-input"
+              onChange={e=> updateCity(e.target.value)}
+            />
           </label><br/>
 
           <label className="line-two-label">
             State: <br/>
-            <input type="text" name="state" className="form-input line-two-input"/>
+            <input
+              type="text"
+              name="state"
+              className="form-input line-two-input"
+              onChange={e=> updateState(e.target.value)}
+            />
           </label><br/>
 
           <label className="line-two-label">
             Founded Date: <br/>
-            <input type="date" name="founded-date" className="form-input line-two-input"/>
+            <input
+              type="date"
+              name="founded-date"
+              className="form-input line-two-input"
+              onChange={e=> updateFounded(e.target.value)}
+            />
           </label>
         </div>
 
         <div className="form-line-three">
           <label>
             Description: <br/>
-            <textarea name="description" className=" form-input line-three-input"/>
+            <textarea
+              name="description"
+              className=" form-input line-three-input"
+              onChange={e=> updateDescription(e.target.value)}
+            />
           </label>
         </div>
+
+        <button className="save-btn" onClick={e => handleFormSubmit(e)}>Save</button>
 
       </form>
 
