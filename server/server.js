241e6f527fcc898f4767f8f52d7adea5f6b6a2c9
Chris Williams <Chris@Chriss-MacBook-Pro-2.local>
2020-11-27 18:08:59 -0700

    creates server routes and server file system

--- server/server.js
+++ server/server.js
@@ -11,6 +11,21 @@ const PORT = process.env.PORT || 3001
 app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
 app.use(bodyParser.json());
 
+app.get('/companies', (req, res) => {
+
+})
+
+app.post('/companies', (req, res) => {
+
+})
+
+app.put('/companies/:company_id', (req, res) => {
+
+})
+
+app.delete('/companies/:company_id', (req, res) => {
+
+})
 
 app.listen(PORT, () => {
   console.log('Server listening on port ', PORT);
