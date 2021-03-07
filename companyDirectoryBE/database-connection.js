a02e391029d310ea7774f3dcbbb7a63b622f0fc3
justcallmebeef <bethanymitch10@gmail.com>
2021-03-06 20:27:45 -0700

    rm be code and images

--- companyDirectoryBE/database-connection.js
+++
@@ -1,7 +0,0 @@
-const environment = process.env.NODE_ENV || "development";
-const config = require("./knexfile");
-const configEnv = config[environment];
-const knex = require("knex");
-const connection = knex(configEnv);
-
-module.exports = connection;
