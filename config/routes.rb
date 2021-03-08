c7f0dc93600e44a680007efe99c2b915e3414a74
sonisahu <sonisahu@outlook.com>
2021-03-08 14:14:43 +0530

    Namespace APIs to /api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :companies do
-    resources :founders, only: :create
+  namespace :api do
+    namespace :v1 do
+      resources :companies do
+        resources :founders, only: :create
+      end
+    end
   end
 end
