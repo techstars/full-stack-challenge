ffd29f9c756f8e949a8acf92d21da75d707dc82c
justcallmebeef <bethanymitch10@gmail.com>
2021-03-10 11:48:16 -0700

    added founder card to company profile and displaying founders for the company

--- company-directory-fe/src/features/CompanyProfile/index.js
+++ company-directory-fe/src/features/CompanyProfile/index.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import DeleteModal from "./DeleteModal";
 import UpdateModal from "./UpdateModal";
+import Founders from "./Founders";
 
 const CompanyProfile = (props) => {
-  const { selected, companiesAPI, setTriggered } = props;
+  const { selected, companiesAPI, foundersAPI, setTriggered } = props;
   const [showDelete, setShowDelete] = useState(false);
   const [showUpdate, setShowUpdate] = useState(false);
 
@@ -34,48 +35,53 @@ const CompanyProfile = (props) => {
 
   return (
     <>
-      <h1 className="text-center m-3">Company Profile</h1>
-      <Card key={selected.id} className="m-3">
-        <Card.Body>
-          <Card.Title>{selected.companyName}</Card.Title>
-          {selected.foundedDate.length > 0 ? (
-            <Card.Subtitle className="mb-2">
-              {displayDate} | {selected.companyCity}, {selected.companyState}
-            </Card.Subtitle>
-          ) : (
-            <Card.Subtitle className="mb-2">
-              {selected.companyCity}, {selected.companyState}
-            </Card.Subtitle>
-          )}
-          <Card.Text>{selected.companyDescription}</Card.Text>
-        </Card.Body>
-        <div className="m-5">
-          <Button className="m-2" onClick={() => setShowUpdate(true)}>
-            Edit
-          </Button>
-          <Button className="m-2" onClick={() => setShowDelete(true)}>
-            Delete
-          </Button>
+      <div className="container">
+        <h1 className="text-center m-3 container">Company Profile</h1>
+        <Card key={selected.id} className="m-3">
+          <Card.Body>
+            <Card.Title>{selected.companyName}</Card.Title>
+            {selected.foundedDate.length > 0 ? (
+              <Card.Subtitle className="mb-2">
+                {displayDate} | {selected.companyCity}, {selected.companyState}
+              </Card.Subtitle>
+            ) : (
+              <Card.Subtitle className="mb-2">
+                {selected.companyCity}, {selected.companyState}
+              </Card.Subtitle>
+            )}
+            <Card.Text>{selected.companyDescription}</Card.Text>
+          </Card.Body>
+          <div className="mr-5 ml-5">
+            <Button className="m-2" onClick={() => setShowUpdate(true)}>
+              Edit
+            </Button>
+            <Button className="m-2" onClick={() => setShowDelete(true)}>
+              Delete
+            </Button>
+          </div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <Founders foundersAPI={foundersAPI} selected={selected} />
+          </div>
+        </Card>
+        <DeleteModal
+          showDelete={showDelete}
+          setShowDelete={setShowDelete}
+          selected={selected}
+          companiesAPI={companiesAPI}
+          setTriggered={setTriggered}
+        />
+        <UpdateModal
+          showUpdate={showUpdate}
+          setShowUpdate={setShowUpdate}
+          selected={selected}
+          companiesAPI={companiesAPI}
+          setTriggered={setTriggered}
+        />
+        <div className="text-center mb-2">
+          <Link to="/">
+            <Button>Back to Company Directory</Button>
+          </Link>
         </div>
-      </Card>
-      <DeleteModal
-        showDelete={showDelete}
-        setShowDelete={setShowDelete}
-        selected={selected}
-        companiesAPI={companiesAPI}
-        setTriggered={setTriggered}
-      />
-      <UpdateModal
-        showUpdate={showUpdate}
-        setShowUpdate={setShowUpdate}
-        selected={selected}
-        companiesAPI={companiesAPI}
-        setTriggered={setTriggered}
-      />
-      <div className="text-center mb-2">
-        <Link to="/">
-          <Button>Back to Company Directory</Button>
-        </Link>
       </div>
     </>
   );
