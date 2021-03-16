fcb6368c12df0a18d1d24338e46cfded4834c239
melissafrasik <80595928+melissafrasik@users.noreply.github.com>
2021-03-16 18:50:05 -0400

    Update connect.js
    updated connection vars

--- api/connect.js
+++ api/connect.js
@@ -1,9 +1,9 @@
 var mysql = require("mysql");
 var pool = mysql.createPool({
-  host: 'eyw6324oty5fsovx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  user: "yoqxd3r3av59i46v",
-  password: "pvu7ypoidoj1xqv6",
-  database: "w5wq7ozfs572jx76",
+  host: process.env.JAWSDB_HOST || 'eyw6324oty5fsovx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user: process.env.JAWSDB_USER || "yoqxd3r3av59i46v",
+  password: process.env.JAWSDB_PASSWORD || "pvu7ypoidoj1xqv6",
+  database: process.env.JAWSDB_DB || "w5wq7ozfs572jx76",
   connectionLimit: 10
 });
 
