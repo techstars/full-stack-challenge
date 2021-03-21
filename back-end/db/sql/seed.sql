6aae293c2f3a19fdb13a66618550c8a89dfb1b10
Eduardo Lopez <eduardo.lpz.t@gmail.com>
2021-03-21 17:27:34 -0500

    added seed.sql file

---
+++ back-end/db/sql/seed.sql
@@ -0,0 +1,109 @@
+INSERT INTO location(id,city, state) VALUES(1,'Boulder', 'Colorado');
+INSERT INTO companies(id, name, description,location_id) VALUES(1, 'Techstars', 'Techstars is an American seed accelerator founded in 2006 in Boulder, Colorado. Since 2006, Techstars has invested in more than 2,200 companies and today has a market cap of $27 Billion. Fewer than 1% of applicants are accepted
+Techstars is a worldwide network that helps entrepreneurs succeed. Their flagship Accelerator Program partners corporations with visionary entrepreneurs to experiment with breakthrough technologies, develop innovative solutions and transform company culture.
+Techstars began with three simple ideas—entrepreneurs create the future, collaboration drives innovation, and great ideas can come from anywhere. Today our mission is to make innovation accessible to everyone, everywhere. We do this by connecting startups, investors, corporations, and cities to create a more sustainable and inclusive world',
+1);
+INSERT INTO founder(full_name, title, company_id) VALUES('Maelle Gavet', 'CEO', 1);
+INSERT INTO founder(full_name, title, company_id) VALUES('David Cohen', 'Chairman', 1);
+
+
+
+INSERT INTO location(id,city, state) VALUES(2,'San Francisco', 'California');
+INSERT INTO companies(id, name, description,location_id) VALUES(2, 'Square', 'Square, Inc. is a financial services company. It provides mobile payments point of sale terminals, particularly including its Square Reader which enables processing bank card payments from smartphones and mobile devices. Square also provides associated payment software, auxiliary equipment, and small business services, including e-commerce solutions, delivery services, business loans, and cash flow tools',
+2);
+INSERT INTO founder(full_name, title, company_id) VALUES('Jack Dorsey', 'CEO', 2);
+INSERT INTO founder(full_name, title, company_id) VALUES('Amy Brooks', 'CIO', 2);
+
+
+
+
+INSERT INTO location(id,city, state) VALUES(3,'Mountain View', 'California');
+INSERT INTO companies(id, name, description,location_id) VALUES(3, 'Intuit', 'Intuit Inc. is a software company specialized in financial, accounting, and tax preparation software and related services. It is especially famous for producing TurboTax, a consumer tax preparation application, QuickBooks, a small business accounting program, and operating Mint, a personal finance application, among others',
+3);
+INSERT INTO founder(full_name, title, company_id) VALUES('Jack Dorsey', 'CEO', 3);
+INSERT INTO founder(full_name, title, company_id) VALUES('Amy Brooks', 'CFO', 3);
+
+
+
+
+INSERT INTO location(id,city, state) VALUES(4,'Santa Clara', 'California');
+INSERT INTO companies(id, name, description,location_id) VALUES(4, 'AMD', 'Advanced Micro Devices, Inc., more simply known as AMD, is a multinational semiconductor company. It develops and manufactures a range of microprocessors and a number of other semiconductor components including motherboard chipsets, embedded processors, and graphics processing units (GPUs) for personal computers, servers, workstations, data centers, and embedded system applications',
+4);
+INSERT INTO founder(full_name, title, company_id) VALUES('Dr. Lisa Su', 'CEO', 4);
+INSERT INTO founder(full_name, title, company_id) VALUES('Rick Bergman', 'CFO', 4);
+
+
+
+INSERT INTO location(id,city, state) VALUES(5,'Santa Clara', 'California');
+INSERT INTO companies(id, name, description,location_id) VALUES(5, 'ServiceNow', 'ServiceNow is a technical management service provider. Functioning as a Platform-as-a-service provider, it delivers services including IT workflows and service management, employee and HR services, customer service management as well as an application building ecosystem. It levies fees with a cost per user structure, allowing businesses of all sizes to use its platform',
+5);
+INSERT INTO founder(full_name, title, company_id) VALUES('Bill McDermott', 'CEO', 5);
+INSERT INTO founder(full_name, title, company_id) VALUES('Chris Bedi', 'CIO', 5);
+
+
+
+
+INSERT INTO location(id,city, state) VALUES(6,'Armonk', 'New York');
+INSERT INTO companies(id, name, description,location_id) VALUES(6, 'IBM', 'Founded in 1911, the International Business Machines Corporation, better known as IBM, is a multinational information technology company operating in more than 170 countries. Manufacturing and marketing computer hardware, middleware, and software and providing hosting and consulting services, IBM is also a major technology research organization',
+6);
+INSERT INTO founder(full_name, title, company_id) VALUES('Arvind Krishna', 'CEO', 6);
+INSERT INTO founder(full_name, title, company_id) VALUES('Gary Cohn', 'Vice Chairman', 6);
+
+
+
+
+INSERT INTO location(id,city, state) VALUES(7,'San Jose', 'California');
+INSERT INTO companies(id, name, description,location_id) VALUES(7, 'Zoom', 'Zoom Video Communications, Inc., more simply known as Zoom, is a communications technology providing HD video, voice, chat, and content sharing. Through its cloud-based peer-to-peer software, Zoom users around the world can benefit from a number of applications including teleconferencing, telecommuting, distance education, and social relations on any internet-connected device',
+7);
+INSERT INTO founder(full_name, title, company_id) VALUES('Eric S. Yuan', 'CEO', 7);
+INSERT INTO founder(full_name, title, company_id) VALUES('Ryan Azus', 'CRO', 7);
+
+
+
+
+
+
+
+
+INSERT INTO location(id,city, state) VALUES(8,'San Francisco', 'California');
+INSERT INTO companies(id, name, description,location_id) VALUES(8, 'Airbnb', 'Airbnb is a marketplace dedicated to connecting travelers to rooms, apartments, houses, and unique accommodations and experiences around the world. It helps list, discover, and book distinctive spaces and facilitates bookings and financial transactions',
+8);
+INSERT INTO founder(full_name, title, company_id) VALUES('Brian Chesky', 'CEO', 8);
+INSERT INTO founder(full_name, title, company_id) VALUES('Nathan Blecharczyk', 'Chairman', 8);
+
+
+
+INSERT INTO location(id,city, state) VALUES(9,'Stamford', 'Connecticut');
+INSERT INTO companies(id, name, description,location_id) VALUES(9, 'Charter Communications', 'Charter Communications, Inc. is a telecommunications and mass media company. Operating under the Spectrum brand, Charter Communications is one of the largest cable operators, pay-TV operators and telephone providers in the United States',
+9);
+INSERT INTO founder(full_name, title, company_id) VALUES('Tom Rutledge', 'CEO', 9);
+INSERT INTO founder(full_name, title, company_id) VALUES('John Bickham', 'President', 9);
+
+
+
+
+
+INSERT INTO location(id,city, state) VALUES(10,'Dallas', 'Texas');
+INSERT INTO companies(id, name, description,location_id) VALUES(10, 'Texas Instruments', 'Texas Instruments Inc., also known as TI, is a company that produces semiconductors and integrated circuits. Tracing its origins to 1930, Texas Instruments is the world’s largest producer of analog technology components, providing analog chips and embedded processors, digital light processing technology, and education technology products such as calculators, microcontrollers, and multi-core processors',
+10);
+INSERT INTO founder(full_name, title, company_id) VALUES('Richard Templeton', 'CEO', 10);
+INSERT INTO founder(full_name, title, company_id) VALUES('Haviv Llan', 'COO', 10);
+
+
+
+
+
+
+INSERT INTO location(id,city, state) VALUES(11,'Bellevue', 'Washington');
+INSERT INTO companies(id, name, description,location_id) VALUES(11, 'T-Mobile', 'T-Mobile US, more simply known as T-Mobile, is a wireless network operator providing wireless voice and data services and host network for multiple mobile virtual network operators. T-Mobile is the third-largest wireless carrier with more than 80 million customers in the US in 2019. Its majority shareholder is Deutsche Telekom, one of the largest German companies',
+11);
+INSERT INTO founder(full_name, title, company_id) VALUES('Mike Sievert', 'CEO', 11);
+INSERT INTO founder(full_name, title, company_id) VALUES('Nestor Cano', 'EVP', 11);
+
+
+
+INSERT INTO location(id,city, state) VALUES(12,'San Diego', 'California');
+INSERT INTO companies(id, name, description,location_id) VALUES(12, 'Qualcomm', 'Qualcomm Inc. is a multinational company providing semiconductors and telecommunications equipment. It develops and commercializes wireless telecommunications products and services, with subsidiaries specializing in certain areas of operations: Qualcomm CDMA Technologies sells Qualcomm’s products and services, Qualcomm Technology Licensing manages the patent licensing business, and Qualcomm Technologies, Inc. conducts Qualcomm’s R&D activities',
+12);
+INSERT INTO founder(full_name, title, company_id) VALUES('Steve Mollenkopf', 'CEO', 12);
+INSERT INTO founder(full_name, title, company_id) VALUES('Cristiano R. Amon', 'President', 12);
