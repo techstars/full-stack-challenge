64b0b93cfa9a6ae987f3855d19487a5247dc3ff5
Eduardo Lopez <eduardo.lpz.t@gmail.com>
2021-03-22 03:48:04 -0500

    testing

--- front-end/src/constants.js
+++ front-end/src/constants.js
@@ -1,4 +1,5 @@
-const API_URL = 'http://143.198.100.192:4000/api';
+const API_URL = 'http://127.0.0.1:4000/api';
+// const API_URL = 'http://143.198.100.192:4000/api';
 
 module.exports = {
     API_URL
