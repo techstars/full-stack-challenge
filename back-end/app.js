5b01846c9ec5565a71d6441662988e221b3a2572
Eduardo Lopez <eduardo.lpz.t@gmail.com>
2021-03-22 03:51:21 -0500

    testing

--- back-end/app.js
+++ back-end/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const router = express.Router();
-const cors = require('cors');
+// const cors = require('cors');
 // const helmet = require('helmet');
 require('dotenv').config();
 
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(logger('dev'));
 // app.use(helmet());
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
